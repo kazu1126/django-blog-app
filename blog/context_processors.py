f297d9c599597ac98573148a608f4c69585ad2c1
kazu <kazu@haobanting.com>
2023-04-02 20:45:53 +0900

    initial commit

---
+++ blog/context_processors.py
@@ -0,0 +1,15 @@
+from django.http import HttpRequest
+from django.db.models import Count, Q
+
+from blog.models import Category, Tag
+
+
+
+def common(request :HttpRequest):
+    context = {
+        'categories': Category.objects.annotate(
+            count=Count('post', Q(post__is_published=True))
+        ),
+        'tags': Tag.objects.all(),
+    }
+    return context
